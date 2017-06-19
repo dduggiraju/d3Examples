6d782dc33a78914a8a92edec1cd798c6549e3766
Dheeraj Duggiraju <dduggira@bechtel.com>
2017-06-19 15:53:42 -0500

    adde  city data

--- 2/script.js
+++ 2/script.js
@@ -16,26 +16,52 @@
         console.log(d3.mean(data, function (el) { return +el.population }));
         console.log(d3.extent(data, function (el) { return +el.population }));
     });
-    d3.csv("cities.csv", function (error, data) { dataViz(data); });
-    function dataViz(incomingData) {
-        d3.select("body").selectAll("div.cities")
+    // d3.csv("cities.csv", function (error, data) { dataViz(data); });
+    // function dataViz(incomingData) {
+    //     d3.select("body").selectAll("div.cities")
+    //         .data(incomingData)
+    //         .enter()
+    //         .append("div")
+    //         .attr("class", "cities")
+    //         .attr("id", function (d, i) { return d.label.trim(); })
+    //         .html(function (d, i) { return d.label; });
+    // }
+
+    d3.csv("cities.csv", function (error, data) { cityDataViz(data); });
+    function cityDataViz(incomingData) {
+        var maxPopulation = d3.max(incomingData, function (el) {
+            return parseInt(el.population);
+        });
+        var yScale = d3.scaleLinear().domain([0, maxPopulation]).range([0, 480]);
+        d3.select("svg").attr("style", "height: 480px; width: 600px;");
+        d3.select("svg")
+            .selectAll("rect")
             .data(incomingData)
             .enter()
-            .append("div")
-            .attr("class", "cities")
-            .attr("id", function (d, i) { return d.label.trim(); })
-            .html(function (d, i) { return d.label; });
+            .append("rect")
+            .attr("width", 50)
+            .attr("height", function (d) { return yScale(parseInt(d.population)); })
+            .attr("x", function (d, i) { return i * 60; })
+            .attr("y", function (d) { return 480 - yScale(parseInt(d.population)); })
+            .style("fill", "blue")
+            .style("stroke", "red")
+            .style("stroke-width", "1px")
+            .style("opacity", 0.25);
     }
-    d3.select("svg")
+
+
+    var yScale = d3.scaleLinear().domain([0, 100, 500]).range([0, 50, 100]).clamp(true);
+    d3.select("#nextG")
         .selectAll("rect")
-        .data([15, 50, 22, 8, 100, 10])
+        .data([14, 68, 24500, 430, 19, 1000, 5555])
         .enter()
         .append("rect")
         .attr("width", 10)
-        .attr("height", function (d) { return d; })
+        .attr("height", function (d) { return yScale(d); })
         .style("fill", "blue")
         .style("stroke", "red")
         .style("stroke-width", "1px")
         .style("opacity", 0.25)
-        .attr("x", function(d,i) {return i * 10});;
+        .attr("x", function (d, i) { return i * 10 })
+        .attr("y", function (d) { return (100 - yScale(d)); });
 }());
