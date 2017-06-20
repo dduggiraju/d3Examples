778ebe63c5671b1de88cb0d7950ab6121e223731
Dheeraj Duggiraju <dduggira@bechtel.com>
2017-06-20 12:17:30 -0500

    moved code and removed comments

--- 2/script.js
+++ 2/script.js
@@ -16,16 +16,6 @@
         console.log(d3.mean(data, function (el) { return +el.population }));
         console.log(d3.extent(data, function (el) { return +el.population }));
     });
-    // d3.csv("cities.csv", function (error, data) { dataViz(data); });
-    // function dataViz(incomingData) {
-    //     d3.select("body").selectAll("div.cities")
-    //         .data(incomingData)
-    //         .enter()
-    //         .append("div")
-    //         .attr("class", "cities")
-    //         .attr("id", function (d, i) { return d.label.trim(); })
-    //         .html(function (d, i) { return d.label; });
-    // }
 
     d3.csv("cities.csv", function (error, data) { cityDataViz(data); });
     function cityDataViz(cityData) {
@@ -48,6 +38,21 @@
             .style("stroke-width", "1px")
             .style("opacity", 0.25);
     }
+
+    var yScale = d3.scaleLinear().domain([0, 100, 500]).range([0, 50, 100]).clamp(true);
+    d3.select("#nextG")
+        .selectAll("rect")
+        .data([14, 68, 24500, 430, 19, 1000, 5555])
+        .enter()
+        .append("rect")
+        .attr("width", 10)
+        .attr("height", function (d) { return yScale(d); })
+        .style("fill", "blue")
+        .style("stroke", "red")
+        .style("stroke-width", "1px")
+        .style("opacity", 0.25)
+        .attr("x", function (d, i) { return i * 10 })
+        .attr("y", function (d) { return (100 - yScale(d)); });
 
     d3.json("tweets.json", function (error, data) { tweetDataViz(data.tweets) });
     function tweetDataViz(tweetData) {
@@ -106,18 +111,4 @@
             .style("stroke", "black")
             .style("stroke-width", "1px");
     }
-    var yScale = d3.scaleLinear().domain([0, 100, 500]).range([0, 50, 100]).clamp(true);
-    d3.select("#nextG")
-        .selectAll("rect")
-        .data([14, 68, 24500, 430, 19, 1000, 5555])
-        .enter()
-        .append("rect")
-        .attr("width", 10)
-        .attr("height", function (d) { return yScale(d); })
-        .style("fill", "blue")
-        .style("stroke", "red")
-        .style("stroke-width", "1px")
-        .style("opacity", 0.25)
-        .attr("x", function (d, i) { return i * 10 })
-        .attr("y", function (d) { return (100 - yScale(d)); });
 }());
