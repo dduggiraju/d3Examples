c58b383c9d330d076ffbac4929aa5d2e5b2a95f8
dheerajduggiraju <dheerajduggiraju@gmail.com>
2017-07-05 21:54:32 -0500

    added maps

--- Maps/3/script.js
+++ Maps/3/script.js
@@ -2,13 +2,40 @@
     'use strict';
     var svg = d3.select('body')
         .append('svg')
-        .attr('height', '100vh')
-        .attr('width', '100vw');
-
-    svg.append('rect')
-        .attr('x', 10)
-        .attr('y', 10)
-        .attr('height', 100)
-        .attr('width', 100);
+        .attr("width", 200)
+        .attr("height", 200);
+    function makeData(n) {
+        var arr = [];
+        var i = 0;
+        for (i = 0; i < n; i++) {
+            arr.push({
+                x: Math.floor((Math.random() * 100) + 1),
+                y: Math.floor((Math.random() * 100) + 1),
+                width: Math.floor((Math.random() * 100) + 1),
+                height: Math.floor((Math.random() * 100) + 1)
+            });
+        }
+        return arr;
+    }
+    var incomingData = makeData(2);
+    drawRects(incomingData);
+    function drawRects(incomingData) {
+        var rect = svg.selectAll('rect')
+            .data(incomingData);
+        rect.enter()
+            .append('rect')
+            .attr('x', function (d) {
+                return d.x;
+            })
+            .attr('y', function (d) {
+                return d.y;
+            })
+            .attr('height', function (d) {
+                return d.height;
+            })
+            .attr('width', function (d) {
+                return d.width;
+            });
+    }
 
 }());
